74e632f8e6c82ac2d96e5d29346896a15ee51339
Nyakio Mburu <nyakio.mburu@gmail.com>
2018-09-20 12:10:49 +0300

    adds four calculator functions and their respective forms

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,58 @@
+// Business (or back-end) logic:
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+// User interface (or front-end) logic:
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+   event.preventDefault();
+   var number1 = parseInt($("#add1").val());
+   var number2 = parseInt($("#add2").val());
+   var result = (add(number1, number2));
+   $("#addition").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("form#subtract").submit(function(event) {
+   event.preventDefault();
+   var number1 = parseInt($("#subtract1").val());
+   var number2 = parseInt($("#subtract2").val());
+   var result = (subtract(number1, number2));
+   $("#subtraction").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("form#multiply").submit(function(event) {
+   event.preventDefault();
+   var number1 = parseInt($("#multiply1").val());
+   var number2 = parseInt($("#multiply2").val());
+   var result = (multiply(number1, number2));
+   $("#multiplication").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("form#divide").submit(function(event) {
+   event.preventDefault();
+   var number1 = parseInt($("#divide1").val());
+   var number2 = parseInt($("#divide2").val());
+   var result = (divide(number1, number2));
+   $("#division").text(result);
+  });
+});
+/*
 var subtract = function(number1, number2) {
 return number1 - number2;
 };
@@ -5,20 +60,4 @@ return number1 - number2;
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 alert(subtract(number1, number2));
-
-/* Could consider alternate final line:
-var result = add(number1, number2
-alert(result)
-*/
-
-
-/*
-var add = function(number1, number2) {
-  return number1 + number2;
-};
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
 */
