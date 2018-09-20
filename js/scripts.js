9a51f7f1571b41f130f1ac2146f91e0d644abdf3
Nyakio Mburu <nyakio.mburu@gmail.com>
2018-09-20 09:41:05 +0300

    substitutes initial addition functionality with that of subtraction

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+var subtract = function(number1, number2) {
+return number1 - number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+alert(subtract(number1, number2));
+
+/* Could consider alternate final line:
+var result = add(number1, number2
+alert(result)
+*/
+
+
+/*
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = add(number1, number2);
+alert(result);
+*/
